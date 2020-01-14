d731ca57a51e2f2abcf044b989d0c0eee2ef8a96
kirito6933 <39739034+kirito6933@users.noreply.github.com>
2020-01-14 17:09:31 +0100

    Docker.sh
    Installation Docker auto version 1.0

---
+++ Docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Installation Docker"
+#Install Clé Docker
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum update && yum install docker-ce docker-ce-cli containerd.io
+systemctl start docker
